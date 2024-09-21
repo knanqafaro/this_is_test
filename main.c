2ca05b576f4b2e8648873edc4c62e4695edd7044
knanqafaro <153326506+knanqafaro@users.noreply.github.com>
2024-09-21 16:09:40 +0400

    Updated the code to print 'Hello School' 32 times.

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 int main()
 {
-  int i;
-  for (i = 0; i < 10; i++)
+  char word[]="Hello School";
+  int word_count;
+  for (word_count = 0; word_count < 32; word_count++)
   {
-    printf("School\n");
+  	printf("\n");
+    printf("------\n");
+    printf(word);
   }
+  printf("\n------");
   return 0;
 }
